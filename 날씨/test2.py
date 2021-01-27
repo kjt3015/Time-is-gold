e372187cf8fc535630ad74f6f40f9e2df30a795b
KIM JUNTAE <kjt3015@naver.com>
2021-01-27 10:59:48 +0900

    f

---
+++ 날씨/test2.py
@@ -0,0 +1,24 @@
+#!/C:/Users/kjt30/AppData/Local/Microsoft/WindowsApps/PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0/bin/python3
+
+print("Content-Type: text/html")
+print()
+print('''<!doctype html>
+<html>
+<head>
+  <title>About Python</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.html">Python</a></h1>
+  <ol>
+    <li><a href="html.html">HTML</a></li>
+    <li><a href="css.html">CSS</a></li>
+    <li><a href="javascript.html">JavaScript</a></li>
+    <li><a href="python.html">Python</a></li>
+  </ol>
+  <h2>Python</h2>
+  <p>Python is a programming language that lets you work more quickly and integrate your systems more effectively.
+You can learn to use Python and see almost immediate gains in productivity and lower maintenance costs.</p>
+</body>
+</html>
+''')
